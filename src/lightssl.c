6ad6cca1fff3c832886843eee09c6f1e166662ba
Nicklas Boman <smurfd@gmail.com>
2021-11-12 22:53:54 +0100

    threads

--- src/lightssl.c
+++ src/lightssl.c
@@ -4,10 +4,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <pthread.h>
 #include "lightssl.h"
 
 // dummy
@@ -28,13 +30,46 @@ int ls_srv_init(const char *host, const char *port) {
   return ssock;
 }
 
-int ls_srv_listen(int ssock) {
-  listen(ssock, 1);
-  int csock = accept(ssock, NULL, NULL);
+void *ls_srv_handler(void *sdesc) {
+  int s = *(int*)sdesc;
+  int rd = 1;
+  char *msg;
+  char cm[2000];
+
+  while (rd) {
+    rd = recv(s, cm, 2000, 0);
+    write(s, cm, strlen(cm));
+    printf("cm = %s\n", cm);
+    memset(cm, '\0', 2000);
+  }
+  // if rd == 0, client disconnect
+  // if rd < 0, received failed
+
+  free(sdesc);
+  close(s);
+  pthread_exit(NULL);
+  return 0;
+}
+
+int ls_srv_listen(int ssock, struct sockaddr *cli) {
+  int csock = 1;
+  int *new_sock;
+  int c = sizeof(struct sockaddr_in);
+  listen(ssock, 3);
+  while(csock) {
+    csock = accept(ssock, (struct sockaddr*)&cli, (socklen_t*)&c);
+    pthread_t sniffer_thread;
+    new_sock = (int*)malloc(sizeof *new_sock);
+    *new_sock = csock;
+    if (pthread_create(&sniffer_thread, NULL, ls_srv_handler, (void*)new_sock) < 0) {
+      // couldnt create thread
+      return 1;
+    }
+  }
   return csock;
 }
 
-void ls_srv_send(int csock, const char* msg) {
+void ls_srv_send(int csock, const char *msg) {
   send(csock, msg, sizeof(msg), 0);
 }
 
@@ -54,11 +89,14 @@ int ls_cli_init(const char *host, const char *port) {
   return csock;
 }
 
-char* ls_cli_recv(int csock, char* data) {
+char* ls_cli_recv(int csock, char *data) {
   recv(csock, data, sizeof(data), 0);
   return data;
 }
 
+void ls_cli_send(int csock, const char *msg) {
+  send(csock, msg, strlen(msg), 0);
+}
 // Example:
 // Server
 // int s = ls_srv_init("127.0.0.1", "12345");
--- src/lightssl.h
+++ src/lightssl.h
@@ -8,11 +8,14 @@
 #define LIGHTSSL_H 1
 
 void ls_init();
+// Server
 int ls_srv_init(const char *host, const char *port);
-int ls_srv_listen(int ssock);
-void ls_srv_send(int csock, const char* msg);
-
+void *ls_srv_handler(void *sdesc);
+int ls_srv_listen(int ssock, struct sockaddr *cli);
+void ls_srv_send(int csock, const char *msg);
+// Client
 int ls_cli_init(const char *host, const char *port);
+void ls_cli_send(int csock, const char *msg);
 char* ls_cli_recv(int csock, char *data);
 
 #endif
