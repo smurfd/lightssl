afa4287211e86887ef3082f56c7a3c15df588bd2
Nicklas Boman <smurfd@gmail.com>
2022-07-10 00:46:39 +0200

    basic python handshake split test to new file

--- src/python/shake.py
+++ src/python/shake.py
@@ -1,9 +1,3 @@
-import platform
-import certifi
-import socket
-import ssl
-import os
-
 def ssl_real_connect(sock, ssock, addr, connect_ex):
   if ssock.server_side:
     raise ValueError("can't connect in server-side mode")
@@ -21,7 +15,7 @@ def ssl_real_connect(sock, ssock, addr, connect_ex):
       sock.connect(addr)
       if not rc: ssock._connected = True
       if ssock.do_handshake_on_connect:
-        ssock.do_handshake()
+        ssl_sock_do_shake(ssock)
         return rc
   except (OSError, ValueError):
     ssock._sslobj = None
@@ -43,65 +37,3 @@ def ssl_getpeercert(ssock, binary_form=False):
 def ssl_sock_do_shake(ssock, block=False):
   ssl_sock_chk_conn(ssock)
   ssock._sslobj.do_handshake()
-
-#
-#
-# trying to rewrite the handshake...
-def test_my():
-  # Context creation
-  sslContext = ssl.SSLContext()
-  sslContext.verify_mode = ssl.CERT_REQUIRED
-
-  # Load the CA certificates used for validating the peer's certificate
-  sslContext.load_verify_locations(cafile=os.path.relpath(certifi.where()),
-    capath=None,cadata=None)
-
-  # Create an SSLSocket
-  ss = socket.socket() # not sure why i cant use this as arg to wrap_socket()
-  secureClientSocket = sslContext.wrap_socket(
-    socket.socket(), do_handshake_on_connect=False)
-
-  # Make the connection
-  ssl_connect(ss, secureClientSocket, ("example.org", 443))
-
-  # Explicit handshake
-  ssl_sock_do_shake(secureClientSocket)
-
-  # Get the certificate of the server
-  assert(ssl_getpeercert(secureClientSocket))
-
-#
-#
-# Default easy handshake, for sanity
-def test_default():
-  # Context creation
-  sslContext = ssl.SSLContext()
-  sslContext.verify_mode = ssl.CERT_REQUIRED
-
-  # Load the CA certificates used for validating the peer's certificate
-  sslContext.load_verify_locations(cafile=os.path.relpath(certifi.where()),
-    capath=None,cadata=None)
-
-  # Create an SSLSocket
-  secureClientSocket = sslContext.wrap_socket(
-    socket.socket(), do_handshake_on_connect=False)
-
-  # Make the connection
-  assert(secureClientSocket.connect(("example.org", 443)) == None)
-
-  # Explicit handshake
-  secureClientSocket.do_handshake()
-
-  # Get the certificate of the server
-  assert(secureClientSocket.getpeercert())
-  secureClientSocket.close()
-  socket.socket().close()
-
-#
-#
-# main
-print("testing default...")
-test_default()
-
-print("testing my...")
-test_my()
---
+++ src/python/test_shake.py
@@ -0,0 +1,70 @@
+import platform
+import certifi
+import socket
+import ssl
+import os
+from shake import *
+
+#
+#
+# trying to rewrite the handshake...
+def test_my():
+  # Context creation
+  sslContext = ssl.SSLContext()
+  sslContext.verify_mode = ssl.CERT_REQUIRED
+
+  # Load the CA certificates used for validating the peer's certificate
+  sslContext.load_verify_locations(cafile=os.path.relpath(certifi.where()),
+    capath=None,cadata=None)
+
+  # Create an SSLSocket
+  ss = socket.socket() # not sure why i cant use this as arg to wrap_socket()
+  secureClientSocket = sslContext.wrap_socket(
+    socket.socket(), do_handshake_on_connect=False)
+
+  # Make the connection
+  ssl_connect(ss, secureClientSocket, ("example.org", 443))
+
+  # Explicit handshake
+  ssl_sock_do_shake(secureClientSocket)
+
+  # Get the certificate of the server
+  assert(ssl_getpeercert(secureClientSocket))
+  secureClientSocket.close()
+  ss.close()
+
+#
+#
+# Default easy handshake, for sanity
+def test_default():
+  # Context creation
+  sslContext = ssl.SSLContext()
+  sslContext.verify_mode = ssl.CERT_REQUIRED
+
+  # Load the CA certificates used for validating the peer's certificate
+  sslContext.load_verify_locations(cafile=os.path.relpath(certifi.where()),
+    capath=None,cadata=None)
+
+  # Create an SSLSocket
+  secureClientSocket = sslContext.wrap_socket(
+    socket.socket(), do_handshake_on_connect=False)
+
+  # Make the connection
+  assert(secureClientSocket.connect(("example.org", 443)) == None)
+
+  # Explicit handshake
+  secureClientSocket.do_handshake()
+
+  # Get the certificate of the server
+  assert(secureClientSocket.getpeercert())
+  secureClientSocket.close()
+  socket.socket().close()
+
+#
+#
+# main
+print("testing default...")
+test_default()
+
+print("testing my...")
+test_my()
