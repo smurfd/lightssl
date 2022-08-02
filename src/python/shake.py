f36ac4826c32987908e386951b7ee0043bf0c142
Nicklas Boman <smurfd@gmail.com>
2022-08-02 21:00:09 +0200

    basic python handshake diagram

--- src/python/shake.py
+++ src/python/shake.py
@@ -1,3 +1,64 @@
+"""
+```
+    |                                                     |                     
+ cli|                                                     |srv                  
+    |                                                     |                     
+                                                                                
+     _____________ [1] TCP HANDSHAKE _____________________                      
+                                                                               |
+     ----- >>> --- [1.1] syn  ------------------ >   ----v                     |
+     v---- <   --- [1.2] ack syn --------------- <<< -----        handled by os|
+     ----- >>> --- [1.3] ack ------------------- >   -----                     |
+                              v                                                |
+                                                                                
+     _____________ [2] TLS HANDSHAKE _____________________                      
+                                                                                
+     ----- >>> --- [2.1] client hi ------------- >   ----v                      
+     ----- <   --- [2.1] server hi ------------- <<< -----                      
+     v---- <   --- [2.2] verify server crt ----- <<< -----                      
+     ----- >>> --- [2.3] client crt ------------ >   -----                      
+     ----- >>> --- [2.4] key exchange ---------- >   -----                      
+     ----- >>> --- [2.5] change cipher spec ---- >   -----                      
+     ----- >>> --- [2.6] client finish --------- >   ----v                      
+     ----- <   --- [2.7] change cipher spec ---- <<< -----                      
+     v---- <   --- [2.8] server finished ------- <<< -----                      
+     =-=-= >>> -=- [2.9] encrypted traffic -=-=- <<< -=-=-                      
+                                                                                
+     ...                                                                        
+     ...                                                                        
+     ...                                                                        
+     ...                                                                        
+```
+[1] https://www.khanacademy.org/computing/computers-and-internet/xcae6f4a7ff015e7d:the-internet/xcae6f4a7ff015e7d:transporting-packets/a/transmission-control-protocol--tcp
+https://en.wikipedia.org/wiki/Handshaking#TCP_three-way_handshake
+
+[2] https://www.khanacademy.org/computing/computers-and-internet/xcae6f4a7ff015e7d:online-data-security/xcae6f4a7ff015e7d:secure-internet-protocols/a/transport-layer-security-protocol-tls
+https://en.wikipedia.org/wiki/Transport_Layer_Security#TLS_handshake
+
+[2.1]
+  prot ver : tls 1.3
+  cipher suite : TLS_RSA_256_SHA
+[2.2]
+  prot ver : tls 1.3
+  cipher suite : TLS_RSA_256_SHA
+  cert : pubkey : 0x123456789abcdef
+[2.3]
+[2.4]
+  cli send pre-master key,
+  encrypted with servers public key 
+  cli calculate shared key from pre-master
+  store preshared key locally
+[2.5]
+[2.6]
+  send "finish" encrypted with calculated share key
+[2.7]
+[2.8]
+  server calculate shared key & try to decrypt clients "finish
+  if successful, send back "finish" encrypted 
+[2.9]
+  cli send data using symmetric encryption and shared key
+"""
+
 # grabbed from https://raw.githubusercontent.com/python/cpython/main/Lib/ssl.py
 
 import socket
