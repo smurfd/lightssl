7bf004651a2bceaa9ef70bca1f41ac77356af24c
Nicklas Boman <smurfd@gmail.com>
2022-08-16 23:34:31 +0200

    very basic handshake no more output

--- src/python/vsh_client.py
+++ src/python/vsh_client.py
@@ -5,14 +5,13 @@ def main():
   sock = vsh.connect('127.0.0.1', 9999, bind=False)
   g, p, priv = vsh.prim(), vsh.prim(), vsh.prim()
 
-  ap = (g ** priv) % p # Alice public key
   # Send parameters then send encrypted data
-  vsh.send(sock, ([str(g), str(p), str(ap)]))
-  bp = int(vsh.recv(sock)) # Receive bobs public key
-  vsh.send(sock, vsh.crypt("Secret1234", str(((bp) ** priv) % p)))
-  print("alis pub :", (g ** priv) % p)
-  print("bobs pub :", bp)
-  print("share :", (bp ** priv) % p)
+  vsh.send(sock, ([str(g), str(p), str((g ** priv) % p)]))
+  vsh.send(sock, vsh.crypt("Secret1", str(((int(vsh.recv(sock))) ** priv) % p)))
   sock.close()
+
+  # (g ** priv) % p = alices public key, g & p shared public values
+  # int(vsh.recv(sock)) = bobs public key
+  # (((int(vsh.recv(sock))) ** priv) % p) = shared secret
 
 main()
