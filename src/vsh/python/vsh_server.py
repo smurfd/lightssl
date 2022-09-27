8184eaaf341eff24c1fc51bef83621f3d8e8767c
Nicklas Boman <smurfd@gmail.com>
2022-09-27 23:21:29 +0200

    vsh python move to won folder

---
+++ src/vsh/python/vsh_server.py
@@ -0,0 +1,28 @@
+import vsh
+
+# Server loop
+def srvloop(): listenloop(vsh.connect('127.0.0.1', 9999, bind=True))
+
+# Loop that listens for connections from the client
+def listenloop(sock):
+  while True: c, addr = sock.accept(); shakeloop(c, vsh.prim()); c.close()
+
+# The loop within the loop that handles the handshake
+def shakeloop(c, priv, shake=False):
+  while True:
+    if not shake:
+      shake = True
+      g, p, ap = map(int, vsh.liteval(vsh.recv(c)))
+      vsh.send(c, str((g ** priv) % p)) # Send bobs public key
+    else: vsh.crypt(vsh.recv(c, b=True), (ap ** priv) % p); break
+    # We break after crypt because handshake is done and data
+    # is transfered encrypted
+
+    # ap = alices public key, g & p shared public values
+    # (g ** priv) % p = bobs public key
+    # (ap ** priv) % p = shared secret
+
+# Main function
+def main(): vsh.worker(srvloop)
+
+main()
