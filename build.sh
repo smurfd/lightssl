f8bb48f100c71c7d32d331019e727329c0130ee9
Nicklas Boman <smurfd@gmail.com>
2022-09-29 20:43:01 +0200

    fix build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 if [ -d "src/lightbig" ]; then
     echo "lightbig already cloned"
+    cd src/lightbig
+    git pull
+    cd ../..
 else
     git clone https://github.com/smurfd/lightbig src/lightbig
 fi
