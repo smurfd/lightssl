ab46f737b6c425f890270d5a28d4c0df0f4e8ba3
Nicklas Boman <smurfd@gmail.com>
2022-10-19 21:04:06 +0200

    maby fix lightbig test flakieness?

--- src/test_lightssl.c
+++ src/test_lightssl.c
@@ -75,7 +75,7 @@ int main(int argc, char **argv) {
       // Big test suite
       big_init_m(3, &ac, &ad, &a1);
       big_alloc_max_m(2, &ac, &ad);
-      for (int j = 0; j < 5; j++) {
+      for (int j = 0; j < 1; j++) {
         for (int i = 0; i < nrt; i++) {
           big_alloc_max_m(1, &a1);
           big_set("0", &a1);
