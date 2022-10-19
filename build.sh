52683c0ffa1f7c2eb5202c03db8eba556bd30334
Nicklas Boman <smurfd@gmail.com>
2022-10-19 21:32:15 +0200

    gut lightbig, see https://github.com/smurfd/lightbig/

--- build.sh
+++ build.sh
@@ -1,11 +1,3 @@
-if [ -d "src/lightbig" ]; then
-    echo "lightbig already cloned"
-    cd src/lightbig
-    git pull
-    cd ../..
-else
-    git clone https://github.com/smurfd/lightbig src/lightbig
-fi
 CC=clang
 meson build
 cd build
