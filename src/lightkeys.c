948343535cc38ed419465901c188ade7eb692fc0
Nicklas Boman <smurfd@gmail.com>
2022-11-09 23:53:02 +0100

    lightssl more planing

--- src/lightkeys.c
+++ src/lightkeys.c
@@ -4,3 +4,8 @@
 // https://www.rfc-editor.org/rfc/rfc4050
 
 // https://github.com/smurfd/lightecdh ?
+
+
+// http://www.secg.org/sec2-v2.pdf
+// http://csrc.nist.gov/publications/fips/fips186-3/fips_186-3.pdf
+// https://www.ietf.org/rfc/rfc4492.txt 
