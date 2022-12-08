445f92cf40f976b791fd782b05e3d84b91f701b2
Nicklas Boman <smurfd@gmail.com>
2022-12-08 22:17:40 +0100

    lightssl rm old example

--- example/build_your_project.sh
+++
@@ -1,8 +0,0 @@
-clang -c -o lighthash.o ../src/lighthash.c -fPIC
-clang -c -o lighthash3.o ../src/lighthash3.c -fPIC
-clang -c -o lightkeys.o ../src/lightkeys.c -fPIC
-clang -c -o lightcrypto.o ../src/lightcrypto.c -fPIC
-clang -c -o lightciphers.o ../src/lightciphers.c -fPIC
-clang example.c -o example lighthash3.o
-./example
-rm -f example *.o
