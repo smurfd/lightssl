57d09c01b3197ce7f1896c7634c8501963ef9991
Nicklas Boman <smurfd@gmail.com>
2022-12-10 23:06:43 +0100

    lightssl fix readme and example

--- src/example/example.c
+++ src/example/example.c
@@ -1,20 +1,15 @@
-#include <time.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
 #include <assert.h>
 #include "../lighthash3.h"
 
 int main() {
- char *s = malloc(128);
- char hash[] = "5c452b35648528cf3a00a42021489011dd455b78fc34190c7680173b2d"
+  char ss[129] = {0};
+  char hash[] = "5c452b35648528cf3a00a42021489011dd455b78fc34190c7680173b2d"
     "cdcc7d61e73d4f2c51051e45d26215f9f7729b8986549e169dcee3280bed61cda25f20";
   uint8_t *smurfd = (uint8_t*)"smurfd";
 
-  lighthash3_hash_new(smurfd, s);
-  printf("s=%s\n", s);
+  lighthash3_hash_new(smurfd, ss);
+  printf("s=%s\n", ss);
   printf("------ // -----\n");
-  assert(strcmp(s, hash) == 0);
-  free(s);
+  assert(strcmp(ss, hash) == 0);
 }
