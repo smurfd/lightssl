31deffe32fc9ab81c41a6b6551a720614a0b64a9
Nicklas Boman <smurfd@gmail.com>
2023-03-02 21:49:16 +0100

    example

--- src/example/example.c
+++ src/example/example.c
@@ -4,9 +4,8 @@
 #include "../lighthash.h"
 
 int main() {
-  char ss[129] = {0};
   char hash[] = "5c452b35648528cf3a00a42021489011dd455b78fc34190c7680173b2dcdcc"
-    "7d61e73d4f2c51051e45d26215f9f7729b8986549e169dcee3280bed61cda25f20";
+    "7d61e73d4f2c51051e45d26215f9f7729b8986549e169dcee3280bed61cda25f20",ss[129];
   uint8_t *smurfd = (uint8_t*)"smurfd";
 
   lh3new(smurfd, ss);
