70400d645250ef4ec50ff7ffd46ee27372797dae
Nicklas Boman <smurfd@gmail.com>
2023-03-02 21:05:33 +0100

    lightkeys remove sleep in test

--- src/tests/test_lightkeys.c
+++ src/tests/test_lightkeys.c
@@ -13,11 +13,11 @@ int main() {
   lkrnd_init((uint64_t)(0xea1 ^ 0x31ee7 ^ 42) | 0xe1ee77ee | 31337);
   for (int i = 0; i < KB; ++i) {usleep(10); h[i] = lkrnd_next();}
 
-  usleep(1); assert(lkmake_keys(pubkey, privkey));
-  usleep(1); assert(lkshar_secr(pubkey, privkey, sec));
-  usleep(1); assert(lksign(privkey, h, sig));
-  usleep(1); assert(lkvrfy(pubkey, h, sig));
-  usleep(1); assert(!lkvrfy(privkey, h, sig)); // Assume failure
+  assert(lkmake_keys(pubkey, privkey));
+  assert(lkshar_secr(pubkey, privkey, sec));
+  assert(lksign(privkey, h, sig));
+  assert(lkvrfy(pubkey, h, sig));
+  assert(!lkvrfy(privkey, h, sig)); // Assume failure
   if (*sec || *sig || *privkey || *pubkey || *h) { } // clear no use warn
   printf("OK\n");
   return 0;
