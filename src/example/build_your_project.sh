f61f26305b138b20b200a5cf29a42116581efd00
Nicklas Boman <smurfd@gmail.com>
2023-03-02 21:21:33 +0100

    example

--- src/example/build_your_project.sh
+++ src/example/build_your_project.sh
@@ -1,8 +1,7 @@
 clang -c -o lighthash.o ../lighthash.c -fPIC -Wall -pedantic -O3
-clang -c -o lighthash3.o ../lighthash3.c -fPIC -Wall -pedantic -O3
 clang -c -o lightkeys.o ../lightkeys.c -fPIC -Wall -pedantic -O3
 clang -c -o lightcrypto.o ../lightcrypto.c -fPIC -Wall -pedantic -O3
 clang -c -o lightciphers.o ../lightciphers.c -fPIC -Wall -pedantic -O3
-clang example.c -o example lighthash3.o -Wall -pedantic -O3
+clang example.c -o example lighthash.o -Wall -pedantic -O3
 ./example
 rm -f example *.o
--- src/example/example.c
+++ src/example/example.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include <assert.h>
 #include <string.h>
-#include "../lighthash3.h"
+#include "../lighthash.h"
 
 int main() {
   char ss[129] = {0};
-  char hash[] = "5c452b35648528cf3a00a42021489011dd455b78fc34190c7680173b2d"
-    "cdcc7d61e73d4f2c51051e45d26215f9f7729b8986549e169dcee3280bed61cda25f20";
+  char hash[] = "5c452b35648528cf3a00a42021489011dd455b78fc34190c7680173b2dcdcc"
+    "7d61e73d4f2c51051e45d26215f9f7729b8986549e169dcee3280bed61cda25f20";
   uint8_t *smurfd = (uint8_t*)"smurfd";
 
-  lighthash3_hash_new(smurfd, ss);
+  lh3new(smurfd, ss);
   printf("s=%s\n", ss);
   printf("------ // -----\n");
   assert(strcmp(ss, hash) == 0);
