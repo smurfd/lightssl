0445292613619518a425f6f9d4259ab96de2fe3d
Nicklas Boman <smurfd@gmail.com>
2023-03-06 19:07:31 +0100

    fix sha hmac tests

--- src/tests/test_lighthash_hmac0.c
+++ src/tests/test_lighthash_hmac0.c
@@ -6,13 +6,14 @@
 #include "../lighthash.h"
 
 int main() {
-  cuc* ka = (cuc*)"\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b"
-    "\x0b\x0b\x0b\x0b\x0b";
-  cc *da = "\x48\x69\x20\x54\x68\x65\x72\x65";
-  int dl = 8, kl = 20, err = lh(da, dl, 1, 0, 0, ka, kl, "87AA7CDEA5EF619D4FF0B"
-    "4241A1D6CB02379F4E2CE4EC2787AD0B30545E17CDEDAA833B7D6B8A702038B274EAEA3F4E"
-    "4BE9D914EEB61F1702E696C203A126854", 64);
-  assert(err == 1);
+  //cuc* ka = (cuc*)"\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b"
+  //  "\x0b\x0b\x0b\x0b\x0b";
+  //cc *da = "\x48\x69\x20\x54\x68\x65\x72\x65";
+  //int dl = 8, kl = 20;
+  assert(lh("\x48\x69\x20\x54\x68\x65\x72\x65", 8, 1, 0, 0, (cuc*)"\x0b\x0b\x0b"
+    "\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b", 20, "87AA7CDEA5EF619D4F"
+    "F0B4241A1D6CB02379F4E2CE4EC2787AD0B30545E17CDEDAA833B7D6B8A702038B274EAEA3"
+    "F4E4BE9D914EEB61F1702E696C203A126854", 64) == 1);
   printf("OK\n");
   return 0;
 }
--- src/tests/test_lighthash_hmac3.c
+++ src/tests/test_lighthash_hmac3.c
@@ -6,12 +6,13 @@
 #include "../lighthash.h"
 
 int main() {
-  cc *da = "Test With Truncation";
-  cuc* ka = (cuc*)"\x0c\x0c\x0c\x0c\x0c\x0c\x0c\x0c\x0c\x0c\x0c\x0c\x0c\x0c\x0c"
-    "\x0c\x0c\x0c\x0c\x0c";
-  int kl = 20, dl = 20, err = lh(da, dl, 1, 0, 0, ka, kl,"415FAD6271580A531D417"
-    "9BC891D87A6", 16);
-  assert(err == 1);
+  //cc *da = "Test With Truncation";
+  //cuc* ka = (cuc*)"\x0c\x0c\x0c\x0c\x0c\x0c\x0c\x0c\x0c\x0c\x0c\x0c\x0c\x0c\x0c"
+  //  "\x0c\x0c\x0c\x0c\x0c";
+  //int kl = 20, dl = 20;
+  assert(lh("Test With Truncation", 20, 1, 0, 0, (cuc*)"\x0c\x0c\x0c\x0c\x0c\x0c"
+    "\x0c\x0c\x0c\x0c\x0c\x0c\x0c\x0c\x0c\x0c\x0c\x0c\x0c\x0c", 20,"415FAD62715"
+    "80A531D4179BC891D87A6", 16) == 1);
   printf("OK\n");
   return 0;
 }
