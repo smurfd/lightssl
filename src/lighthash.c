19c1fa4d735c8a4bc3ee28df2416e148e96daf90
Nicklas Boman <smurfd@gmail.com>
2023-03-11 15:53:17 +0100

    shake fix

--- src/lighthash.c
+++ src/lighthash.c
@@ -578,18 +578,16 @@ uint8_t lh3shake_touch(uint8_t *sm, uint8_t s[200], uint8_t next, bool upd) {
 }
 
 void lh3shake_test() {
-  uint8_t *buf = malloc(200*sizeof(uint8_t)), *str = malloc(200*sizeof(uint8_t)),ref[32] = {0}, next = 0, s[200] = {0};
+  uint8_t *buf = malloc(200*sizeof(uint8_t)), *str = malloc(200*sizeof(uint8_t)),ref[32] = {0}, next = 0, next2 = 0, s[200] = {0};
   char sss[64], ccc[2] = {0}, ss[] = "6a1a9d7846436e4dca5728b6f760eef0ca92bf0be5615e96959d767197a0beeb";
 
   memset(buf, 0xA3, 20);
   for (int j = 0; j < 200; j += 20) {next = lh3shake_touch(str, buf, next, true);}
-  next = 0;
   lh3shake_xof(str, &s);
-
   for (int i = 0; i < 32; i++) s[i] = str[i];
-  for (int j = 0; j < 512; j += 32) {next = lh3shake_touch(str, s, next, false);}
+  for (int j = 0; j < 512; j += 32) {next2 = lh3shake_touch(str, s, next2, false);}
   lh3bit2str(s, sss);
-  for (int i = 0; i < 64; i++) {printf("%d  %d\n", sss[i], ss[i]); assert(sss[i] == ss[i]);}
-  // print until we fix fxxin assert working with cmake release/debug
+  for (int i = 0; i < 64; i++) {assert(sss[i] == ss[i]);}
+  // verified assert via debug mode
   free(buf);
 }
--- src/tests/test_lighthash3.c
+++ src/tests/test_lighthash3.c
@@ -6,14 +6,13 @@
 
 int main() {
   uint8_t *smurfd = (uint8_t*)"smurfd";
-  char s[128] = {0};
+  char s[256] = {0};
 
   lh3new(smurfd, s);
-  assert(strcmp(s, "5c452b35648528cf3a00a42021489011dd455b78fc34190c7680173b2dc"
-  "dcc7d61e73d4f2c51051e45d26215f9f7729b8986549e169dcee3280bed61cda25f20")== 0);
-  assert(strcmp(s, "5c452b35648528cf3a00a42021489011dd455b78fc34190c7680173b2dc"
-  "dcc7d61e73d4f2c51051e45d26215f9f7729b8986549e169dcee3280bed61cdffffff")!= 0); // Assume failure
-
+  assert(strcmp(s, "5c452b35648528cf3a00a42021489011dd455b78fc34190c7680173b2dc\
+dcc7d61e73d4f2c51051e45d26215f9f7729b8986549e169dcee3280bed61cda25f20") == 0);
+  assert(strcmp(s, "5c452b35648528cf3a00a42021489011dd455b78fc34190c7680173b2dc\
+dcc7d61e73d4f2c51051e45d26215f9f7729b8986549e169dcee3280bed61cdffffff")!= 0); // Assume failure
   lh3shake_test();
   printf("OK\n");
   return 0;
