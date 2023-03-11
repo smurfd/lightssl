39f023a693a4b0aa639251f2f4bd0c05c3a0239a
Nicklas Boman <smurfd@gmail.com>
2023-03-11 17:25:57 +0100

    separate build release and debug

--- build.sh
+++ build.sh
@@ -1,14 +1,9 @@
 rm -rf build
-cmake -DCMAKE_BUILD_TYPE=Release -Bbuild -DCMAKE_C_COMPILER=clang
+# Build release
+cmake -DCMAKE_BUILD_TYPE=Release -Bbuild/release -DCMAKE_C_COMPILER=clang
 sh ./src/example/gen_cert.sh
-make -Cbuild
-make -Cbuild test
-
-# preparing separate release and debug build
-#cmake -DCMAKE_BUILD_TYPE=Release -Bbuild/release -DCMAKE_C_COMPILER=clang
-#sh ./src/example/gen_cert.sh
-#make -Cbuild/release
-#make -Cbuild/release test
+make -Cbuild/release
+make -Cbuild/release test
 
 #cmake -DCMAKE_BUILD_TYPE=Debug -Bbuild/debug -DCMAKE_C_COMPILER=clang
 #sh ./src/example/gen_cert.sh
