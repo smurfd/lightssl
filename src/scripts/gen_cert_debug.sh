e9cde6f545ab504ec0d74e69bd5a3fd22ba79015
Nicklas Boman <smurfd@gmail.com>
2023-04-01 01:17:51 +0200

    add gen of secp keys

--- src/scripts/gen_cert_debug.sh
+++ src/scripts/gen_cert_debug.sh
@@ -6,3 +6,5 @@ openssl x509 -in `pwd`/build/debug/ca.crt -text -noout &> /dev/null
 
 echo "66612345678966601234567890666" | openssl cms -EncryptedData_encrypt -aes128 -secretkey 31337DEADBEEF666999ABCDEF31337FF -outform der > `pwd`/build/debug/ca128.cms
 echo "66612345678966601234567890666" | openssl cms -EncryptedData_encrypt -aes256 -secretkey 31337DEADBEEF666999ABCDEF31337FF31337DEADBEEF666999ABCDEF31337FF -outform der > `pwd`/build/debug/ca256.cms
+
+openssl ecparam -name secp384r1 -genkey -noout -out `pwd`/build/debug/secp384r1-key.pem
--- src/scripts/gen_cert_release.sh
+++ src/scripts/gen_cert_release.sh
@@ -6,3 +6,5 @@ openssl x509 -in `pwd`/build/release/ca.crt -text -noout &> /dev/null
 
 echo "66612345678966601234567890666" | openssl cms -EncryptedData_encrypt -aes128 -secretkey 31337DEADBEEF666999ABCDEF31337FF -outform der > `pwd`/build/release/ca128.cms
 echo "66612345678966601234567890666" | openssl cms -EncryptedData_encrypt -aes256 -secretkey 31337DEADBEEF666999ABCDEF31337FF31337DEADBEEF666999ABCDEF31337FF -outform der > `pwd`/build/release/ca256.cms
+
+openssl ecparam -name secp384r1 -genkey -noout -out `pwd`/build/release/secp384r1-key.pem
