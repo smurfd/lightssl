6a83ab5faf19c07f7a66a5a154f54e4d89664bff
Nicklas Boman <smurfd@gmail.com>
2023-04-01 23:42:44 +0200

    remove openssl cnf

--- src/scripts/build.sh
+++ src/scripts/build.sh
@@ -1,13 +1,11 @@
 # Run ./src/scripts/build.sh from source root
 rm -rf build
 mkdir -p build/debug build/release
-sh ./src/scripts/gen_cert_cnf.sh
 # Build release
 cmake -DCMAKE_BUILD_TYPE=Release -Bbuild/release -DCMAKE_C_COMPILER=clang
 sh ./src/scripts/gen_cert_release.sh
 make -Cbuild/release
 make -Cbuild/release test
-
 # Build debug, here asserts work
 cmake -DCMAKE_BUILD_TYPE=Debug -Bbuild/debug -DCMAKE_C_COMPILER=clang
 sh ./src/scripts/gen_cert_debug.sh
