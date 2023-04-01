d4b7c1e862fe6f997a6882692ca95d2d7e0d5433
Nicklas Boman <smurfd@gmail.com>
2023-04-01 23:36:16 +0200

    remove old openssl req

--- src/scripts/gen_cert_debug.sh
+++ src/scripts/gen_cert_debug.sh
@@ -1,9 +1,4 @@
 # debug
-#openssl req -x509 -sha256 -days 3650 -newkey rsa:4096 \
-#  -config `pwd`/build/ca.cnf -keyout `pwd`/build/debug/ca.key -out `pwd`/build/debug/ca.crt &> /dev/null
-
-#openssl x509 -in `pwd`/build/debug/ca.crt -text -noout &> /dev/null
-
 echo "66612345678966601234567890666" | openssl cms -EncryptedData_encrypt -aes128 -secretkey 31337DEADBEEF666999ABCDEF31337FF -outform der > `pwd`/build/debug/ca128.cms
 echo "66612345678966601234567890666" | openssl cms -EncryptedData_encrypt -aes256 -secretkey 31337DEADBEEF666999ABCDEF31337FF31337DEADBEEF666999ABCDEF31337FF -outform der > `pwd`/build/debug/ca256.cms
 
