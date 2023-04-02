7178df9b09324983324cde701092bfd703510d92
Nicklas Boman <smurfd@gmail.com>
2023-04-02 18:51:01 +0200

    rewrote crypto test a bit more

--- src/tests/test_lightcrypto.c
+++ src/tests/test_lightcrypto.c
@@ -11,9 +11,9 @@ int main(int argc, char **argv) {
   uint8_t s2[] = "smurfd and more stuff", s3[257], s4[LEN];
   int d = 0;
 
-  if (argc < 2) {
-    lchandle_cert("ca.key", data);
-    assert(lchandle_asn("ca128.cms") == 0); assert(lchandle_asn("ca256.cms") == 0);
+  if (argc == 1) {
+    lchandle_cert("build/debug/ca.key", data);
+    assert(lchandle_asn("build/debug/ca256.cms") == 0);
   } else {lchandle_cert(argv[1], data); assert(lchandle_asn(argv[2]) == 0);}
   lcdecode64(s0, strlen(s0), &d, s3);
   lcencode64(s2, strlen("smurfd and more stuff"), &d, s1);
