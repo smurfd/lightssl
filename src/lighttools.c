ad45e69587bc33a94e9fc7d95b8fe7fa352c932e
Nicklas Boman <smurfd@gmail.com>
2023-04-04 22:00:40 +0200

    tools tiny more readable

--- src/lighttools.c
+++ src/lighttools.c
@@ -3,11 +3,13 @@
 #include "lighttools.h"
 
 static uint32_t oct(int i, int inl, cuc d[257]) {
-  if (i < inl) {return d[i];} else {return 0;}
+  if (i < inl) return d[i];
+  return 0;
 }
 
 static uint32_t sex(cc d[257], char c[257], int i) {
-  if (d[i] == '=') {return 0 & i++;} else {return c[(int)d[i]];}
+  if (d[i] == '=') return (0 & i++);
+  return c[(int)d[i]];
 }
 
 //
@@ -43,7 +45,7 @@ uint32_t utf8dec(uint32_t c) {
 
   if (c > 0x7F) {
     m = (c <= n[0]) ? n[1] : n[2];
-    c = ((c & n[3]) >> 6) | ((c & m ) >> 4) | ((c & n[4]) >> 2) | (c & n[5]);
+    c = ((c & n[3]) >> 6) | ((c & m) >> 4) | ((c & n[4]) >> 2) | (c & n[5]);
   }
   return c;
 }
@@ -56,23 +58,30 @@ uint32_t utf8enc(uint32_t c) {
 
   if (c > 0x7F) {
     m = (c & n[0]) | (c & n[1]) << 2 | (c & n[2]) << 4 | (c & n[3]) << 6;
-    if (c < n[4]) m |= n[5]; else if (c < n[6]) m |= n[7]; else m |= n[8];
+    if (c < n[4]) m |= n[5];
+    else if (c < n[6]) m |= n[7];
+    else m |= n[8];
   }
   return m;
 }
 
+//
+// Base64 encoder
 int base64enc(cuc *data, int inl, char ed[]) {
   int tab[] = {0, 2, 1}, ol = 4 * ((inl + 2) / 3);
 
   for (int i = 0, j = 0; i < inl;) {
     uint32_t a = oct(i++, inl, data), b = oct(i++, inl, data);
     uint32_t c = oct(i++, inl, data), tri = (a << 0x10) + (b << 0x08) + c;
-    for (int k = 3; k >=0; k--) {ed[j++] = enc[(tri >> k * 6) & 0x3F];}
+    for (int k = 3; k >=0; k--) ed[j++] = enc[(tri >> k * 6) & 0x3F];
   }
-  for (int i = 0; i < tab[inl % 3]; i++) ed[ol - 1 - i] = '='; ed[ol] = '\0';
+  for (int i = 0; i < tab[inl % 3]; i++) ed[ol - 1 - i] = '=';
+  ed[ol] = '\0';
   return ol;
 }
 
+//
+// Base64 decoder
 int base64dec(cc *data, int inl, uint8_t dd[]) {
   static char dec[LEN] = {0};
   int ol = inl / 4 * 3;
@@ -83,13 +92,13 @@ int base64dec(cc *data, int inl, uint8_t dd[]) {
     uint32_t a = sex(data, dec, i++), b = sex(data, dec, i++);
     uint32_t c = sex(data, dec, i++), d = sex(data, dec, i++);
     uint32_t tri = (a << 3 * 6) + (b << 2 * 6) + (c << 1 * 6) + (d << 0 * 6);
-    if (j < ol) {for (int k = 2; k >= 0; k--) dd[j++] = (tri >> k * 8) & 0xFF;}
+    if (j < ol) for (int k = 2; k >= 0; k--) dd[j++] = (tri >> k * 8) & 0xFF;
   }
   return ol;
 }
 
 //
-//
+// "Randomizer"
 int lrand(u64 h[KB], u64 k[KB]) {
   prng_init((u64)(0xea1 ^ 0x31ee7 ^ 42) | 0xe1ee77ee | 31337);
   for (int i = 0; i < KB; ++i) {h[i] = prng_next(); k[i] = prng_next();}
@@ -131,15 +140,6 @@ void bit_unpack(uint8_t byte[48], const u64 big[6]) {
   }
 }
 
-//
-// n[i] = ((u64)d[0] << 56) |
-//        ((u64)d[1] << 48) |
-//        ((u64)d[2] << 40) |
-//        ((u64)d[3] << 32) |
-//        ((u64)d[4] << 24) |
-//        ((u64)d[5] << 16) |
-//        ((u64)d[6] << 8)  |
-//         (u64)d[7];
 void bit_pack64(u64 n[DI], const u64 b[KB]) {
   for(u64 i = 0; i < DI; ++i) {
     const u64 *d = b + 8 * (DI - 1 - i); n[i] = 0;
@@ -147,15 +147,6 @@ void bit_pack64(u64 n[DI], const u64 b[KB]) {
   }
 }
 
-//
-// d[0] = n[i] >> 56;
-// d[1] = n[i] >> 48;
-// d[2] = n[i] >> 40;
-// d[3] = n[i] >> 32;
-// d[4] = n[i] >> 24;
-// d[5] = n[i] >> 16;
-// d[6] = n[i] >> 8;
-// d[7] = n[i];
 void bit_unpack64(u64 b[KB], const u64 n[DI]) {
   for(u64 i = 0; i < DI; ++i) {
     u64 *d = b + 8 * (DI - 1 - i);
