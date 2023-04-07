71d8ec850430d9677121bcbaf011ebff302a21e3
Nicklas Boman <smurfd@gmail.com>
2023-04-07 23:52:50 +0200

    666

--- src/tests/test_lighthash3_shake.c
+++ src/tests/test_lighthash3_shake.c
@@ -12,12 +12,15 @@ int main(void) {
 67197a0beeb";
 
   memset(buf, 0xA3, 20);
-  for (int i = 0; i < 200; i += 20) {next = hash_shake_touch(str,buf,next,true);}
+  for (int i = 0; i < 200; i += 20)
+    next = hash_shake_touch(str, buf, next, true);
   hash_shake_xof(str);
   for (int i = 0; i < 32; i++) s[i] = str[i];
-  for (int i = 0; i < 512; i += 32) {next2 = hash_shake_touch(str,s,next2,false);}
+  for (int i = 0; i < 512; i += 32)
+    next2 = hash_shake_touch(str, s, next2, false);
   bit_hex_str(sss, s);
-  for (int i = 0; i < 64; i++) {assert(sss[i] == ss[i]);}
+  for (int i = 0; i < 64; i++)
+    assert(sss[i] == ss[i]);
   if (*ss) {} // get rid of not used var warning
   printf("OK\n");
 }
