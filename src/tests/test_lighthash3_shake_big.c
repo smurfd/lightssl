1cfb6b434a28741f025a99bee439b6515ddf3a15
Nicklas Boman <smurfd@gmail.com>
2023-05-01 23:38:36 +0200

    hash test rewrite little

--- src/tests/test_lighthash3_shake_big.c
+++ src/tests/test_lighthash3_shake_big.c
@@ -7,20 +7,19 @@
 #include "../lighttools.h"
 
 int main(void) {
-  uint8_t out_gold1[512], in_gold1[1024], out_gold2[512], in_gold2[1024];
   uint8_t res[] = {0x75, 0x74, 0x60, 0x89, 0x24, 0x0d, 0x9e, 0x39, 0xff, 0xf1, 0xb4, 0xba, 0x58, 0x13, 0x0a, 0xf5, 0xb9,
        0x74, 0x4f, 0x41, 0x2a, 0x9d, 0xff, 0x73, 0x84, 0x70, 0xd1, 0x24, 0x72, 0x53, 0xd3, 0x2c, 0xe7, 0xfe, 0x5a, 0xef,
        0x0d, 0x43, 0xda, 0x15, 0x5f, 0x29, 0x08, 0x58, 0xa4, 0x2e, 0xa0, 0x41, 0xd3, 0x9a, 0x6b, 0xfd, 0x04, 0x21, 0xd4,
-       0x49, 0x8e, 0xa4, 0x95, 0xbd, 0x41, 0x3a, 0x9f, 0x58};
+       0x49, 0x8e, 0xa4, 0x95, 0xbd, 0x41, 0x3a, 0x9f, 0x58}, in1[1024], in2[1024], out1[512], out2[512];
   char s[130] = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et\
  dolore magna aliqua. Ut eni";
 
-  for (int i = 0; i < 130; i++) {in_gold1[i] = s[i]; in_gold2[i] = s[i];}
-  hash_shake_new(out_gold1, 64, in_gold1, 130);
-  hash_shake_new(out_gold2, 64, in_gold2, 130);
-
-  for (int i = 0; i < 64; i++) {printf("%02x %02x %02x\n", out_gold1[i], out_gold2[i], res[i]);}
-  for (int i = 0; i < 64; i++) {assert(out_gold1[i] == res[i]); assert(out_gold2[i] == res[i]);}
+  memcpy(in1, s, 130 * sizeof(uint8_t)); memcpy(in2, s, 130 * sizeof(uint8_t));
+  hash_shake_new(out1, 64, in1, 130); hash_shake_new(out2, 64, in2, 130);
+  for (int i = 0; i < 64; i++) {
+    printf("%02x %02x %02x\n", out1[i], out2[i], res[i]);
+    assert(out1[i] == res[i]); assert(out2[i] == res[i]);
+  }
   if ((*res)) {} // get rid of not used var warning
 
   printf("OK\n");
--- src/tests/test_lighthash3_shake_ref.c
+++ src/tests/test_lighthash3_shake_ref.c
@@ -7,19 +7,18 @@
 #include "../lighttools.h"
 
 int main(void) {
-  uint8_t out_gold1[512], in_gold1[1024], out_gold2[512], in_gold2[1024];
   uint8_t res[] = {0xf6, 0x49, 0x68, 0x85, 0x8b, 0x5c, 0xd8, 0xa6, 0x4f, 0xfd, 0xd9, 0x2e, 0x8c, 0x72, 0xda, 0x03, 0x87,
        0xc5, 0x68, 0x9b, 0x56, 0x2e, 0x96, 0x28, 0x86, 0x04, 0xdf, 0x95, 0x31, 0x5f, 0xee, 0xfa, 0x5a, 0xe9, 0xf0, 0x59,
        0x6b, 0x0b, 0x3d, 0x47, 0xcd, 0x61, 0xac, 0x67, 0x6a, 0xd1, 0xfb, 0x20, 0xcf, 0x3d, 0x92, 0xab, 0x2b, 0x68, 0xda,
-       0xa4, 0x89, 0x31, 0xcc, 0x58, 0xd6, 0xd7, 0x23, 0xc7};
+       0xa4, 0x89, 0x31, 0xcc, 0x58, 0xd6, 0xd7, 0x23, 0xc7}, in1[1024], in2[1024], out1[512], out2[512];
   char s[] = "\xa3\xa3\xa3\xa3\xa3\xa3\xa3\xa3\xa3\xa3\xa3\xa3\xa3\xa3\xa3\xa3\xa3\xa3\xa3\xa3";
 
-  for (int i = 0; i < 20; i++) {in_gold1[i] = s[i]; in_gold2[i] = s[i];}
-  hash_shake_new(out_gold1, 64, in_gold1, 20);
-  hash_shake_new(out_gold2, 64, in_gold2, 20);
-
-  for (int i = 0; i < 64; i++) {printf("%02x %02x %02x\n", out_gold1[i], out_gold2[i], res[i]);}
-  for (int i = 0; i < 64; i++) {assert(out_gold1[i] == res[i]); assert(out_gold2[i] == res[i]);}
+  memcpy(in1, s, 20 * sizeof(uint8_t)); memcpy(in2, s, 20 * sizeof(uint8_t));
+  hash_shake_new(out1, 64, in1, 20); hash_shake_new(out2, 64, in2, 20);
+  for (int i = 0; i < 64; i++) {
+    printf("%02x %02x %02x\n", out1[i], out2[i], res[i]);
+    assert(out1[i] == res[i]); assert(out2[i] == res[i]);
+  }
   if ((*res)) {} // get rid of not used var warning
   printf("OK\n");
 }
