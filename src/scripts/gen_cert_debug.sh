0b26978e506b3aedc7e8cc83eb0a202c7363479a
Nicklas Boman <smurfd@gmail.com>
2024-08-23 22:12:08 +0200

    makefile mkdir in gen cert script

--- src/scripts/gen_cert_debug.sh
+++ src/scripts/gen_cert_debug.sh
@@ -1,4 +1,5 @@
 # debug
+mkdir -p `pwd`/build/debug/ 
 echo "66612345678966601234567890666" | openssl cms -EncryptedData_encrypt -aes128 -secretkey 31337DEADBEEF666999ABCDEF31337FF -outform der > `pwd`/build/debug/ca128.cms
 echo "66612345678966601234567890666" | openssl cms -EncryptedData_encrypt -aes256 -secretkey 31337DEADBEEF666999ABCDEF31337FF31337DEADBEEF666999ABCDEF31337FF -outform der > `pwd`/build/debug/ca256.cms
 
--- src/scripts/gen_cert_release.sh
+++ src/scripts/gen_cert_release.sh
@@ -1,4 +1,5 @@
 # release
+mkdir -p `pwd`/build/release/ 
 echo "66612345678966601234567890666" | openssl cms -EncryptedData_encrypt -aes128 -secretkey 31337DEADBEEF666999ABCDEF31337FF -outform der > `pwd`/build/release/ca128.cms
 echo "66612345678966601234567890666" | openssl cms -EncryptedData_encrypt -aes256 -secretkey 31337DEADBEEF666999ABCDEF31337FF31337DEADBEEF666999ABCDEF31337FF -outform der > `pwd`/build/release/ca256.cms
 
